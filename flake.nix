116b5557a58f0b534a6c3f902b60e4dc1044ab07
Fenhl <fenhl@fenhl.net>
2026-06-10 14:08:31 +0000

    nix: Shell completion

--- flake.nix
+++ flake.nix
@@ -34,13 +34,24 @@
             manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
         in {
             default = pkgs.rustPlatform.buildRustPackage {
+                pname = "playlist";
+                version = manifest.version;
                 cargoLock = {
                     allowBuiltinFetchGit = true; # allows omitting cargoLock.outputHashes
                     lockFile = ./Cargo.lock;
                 };
-                pname = "playlist";
+                nativeBuildInputs = with pkgs; [
+                    installShellFiles # required for `installShellCompletion` in postInstall hook
+                ];
+                postInstall = let
+                    playlist = "${pkgs.stdenv.hostPlatform.emulator pkgs.buildPackages} $out/bin/playlist";
+                in pkgs.lib.optionalString (pkgs.stdenv.hostPlatform.emulatorAvailable pkgs.buildPackages) ''
+                    installShellCompletion --cmd playlist \
+                        --bash <(COMPLETE=bash ${playlist}) \
+                        --fish <(COMPLETE=fish ${playlist}) \
+                        --zsh <(COMPLETE=zsh ${playlist})
+                '';
                 src = ./.;
-                version = manifest.version;
             };
         });
     };
