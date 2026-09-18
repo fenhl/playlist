f2673edbf6171a8cd9da2ff772c9e5a9ba352be8
Fenhl <fenhl@fenhl.net>
2026-09-18 23:32:59 +0000

    Use a helper to reduce flake boilerplate

--- flake.nix
+++ flake.nix
@@ -1,58 +1,42 @@
 {
     inputs = {
-        nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+        flake.url = "github:fenhl/flake";
         rust-overlay = {
             url = "github:oxalica/rust-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.nixpkgs.follows = "flake/nixpkgs";
         };
     };
-    outputs = attrs: let
-        supportedSystems = [
-            "aarch64-darwin"
-            "aarch64-linux"
-            "x86_64-darwin"
-            "x86_64-linux"
+    outputs = attrs: attrs.flake.lib {
+        overlays = [
+            attrs.rust-overlay.overlays.default
         ];
-        forEachSupportedSystem = f: attrs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-            pkgs = import attrs.nixpkgs {
-                inherit system;
-                overlays = [
-                    attrs.rust-overlay.overlays.default
-                ];
-            };
-        });
-    in {
-        devShells = forEachSupportedSystem({ pkgs, ... }: {
-            pre-commit = pkgs.mkShell {
-                packages = with pkgs; [
-                    cargo-deny
-                    rust-bin.nightly.latest.default # nightly cargo, required to run the pre-commit script
-                ];
-            };
-        });
-        packages = forEachSupportedSystem ({ pkgs, ... }: let
+        devShells.pre-commit = { pkgs, ... }: {
+            packages = with pkgs; [
+                cargo-deny
+                rust-bin.nightly.latest.default # nightly cargo, required to run the pre-commit script
+            ];
+        };
+        packages.default = { pkgs, ... }: let
             manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        in {
-            default = pkgs.rustPlatform.buildRustPackage {
-                pname = "playlist";
-                version = manifest.version;
-                cargoLock = {
-                    allowBuiltinFetchGit = true; # allows omitting cargoLock.outputHashes
-                    lockFile = ./Cargo.lock;
-                };
-                nativeBuildInputs = with pkgs; [
-                    installShellFiles # required for `installShellCompletion` in postInstall hook
-                ];
-                postInstall = let
-                    playlist = "${pkgs.stdenv.hostPlatform.emulator pkgs.buildPackages} $out/bin/playlist";
-                in pkgs.lib.optionalString (pkgs.stdenv.hostPlatform.emulatorAvailable pkgs.buildPackages) ''
-                    installShellCompletion --cmd playlist \
-                        --bash <(COMPLETE=bash ${playlist}) \
-                        --fish <(COMPLETE=fish ${playlist}) \
-                        --zsh <(COMPLETE=zsh ${playlist})
-                '';
-                src = ./.;
+        in pkgs.rustPlatform.buildRustPackage {
+            inherit (manifest) version;
+            pname = "playlist";
+            cargoLock = {
+                allowBuiltinFetchGit = true; # allows omitting cargoLock.outputHashes
+                lockFile = ./Cargo.lock;
             };
-        });
+            nativeBuildInputs = with pkgs; [
+                installShellFiles # required for `installShellCompletion` in postInstall hook
+            ];
+            postInstall = let
+                playlist = "${pkgs.stdenv.hostPlatform.emulator pkgs.buildPackages} $out/bin/playlist";
+            in pkgs.lib.optionalString (pkgs.stdenv.hostPlatform.emulatorAvailable pkgs.buildPackages) ''
+                installShellCompletion --cmd playlist \
+                    --bash <(COMPLETE=bash ${playlist}) \
+                    --fish <(COMPLETE=fish ${playlist}) \
+                    --zsh <(COMPLETE=zsh ${playlist})
+            '';
+            src = ./.;
+        };
     };
 }
